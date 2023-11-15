74d16cd74d0a3700e8dd0b4308b344454b84d480
Vishal-dev97 <vishaltakale56@gmail.com>
2023-11-15 20:08:08 +0530

    graph in black theme

--- src/TradingViewWidget.js
+++ src/TradingViewWidget.js
@@ -28,20 +28,21 @@ export default function TradingViewWidget() {
       return () => onLoadScriptRef.current = null;
 
       function createWidget() {
-        if (document.getElementById('tradingview_e3878') && 'TradingView' in window) {
+        if (document.getElementById('tradingview_e0d83') && 'TradingView' in window) {
           new window.TradingView.widget({
-            autosize: true,
-            symbol: "NSE:NIFTY",
+            autosize: false,
+            symbol: "NASDAQ:AAPL",
             interval: "D",
             timezone: "Etc/UTC",
             theme: "dark",
             style: "1",
             locale: "in",
             enable_publishing: false,
-            backgroundColor: "rgba(255, 242, 204, 1)",
+            backgroundColor: "rgba(0, 0, 0, 1)",
             gridColor: "rgba(0, 0, 0, 0.06)",
+            withdateranges: true,
             allow_symbol_change: true,
-            container_id: "tradingview_e3878"
+            container_id: "tradingview_e0d83"
           });
         }
       }
@@ -51,9 +52,9 @@ export default function TradingViewWidget() {
 
   return (
     <div className='tradingview-widget-container' style={{ height: "100%", width: "100%" }}>
-      <div id='tradingview_e3878' style={{ height: "calc(100% - 100px)", width: "100%" }} />
+      <div id='tradingview_e0d83' style={{ height: "calc(100% - 32px)", width: "100%" }} />
       <div className="tradingview-widget-copyright">
-        {/* <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a> */}
+        <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
       </div>
     </div>
   );
